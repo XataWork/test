9a4705b886d4389f16318087f3bc2966f0230e44
boozzd <shapovdu@yandex.ru>
2014-06-14 20:34:51 +0300

    first commit xatawork

---
+++ ajax.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 14.06.14
+ * Time: 20:13
+ */
+$a = $_POST['data'];
+$temp = array(
+    'test' => $a,
+    'test1' => 'blablabla'
+);
+header('Content-Type: application/json');
+echo json_encode($temp);
--- index.php
+++ index.php
@@ -1,15 +1,37 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 14.06.14
- * Time: 19:46
- */
-phpinfo();
-echo 'Hellow world!!!';
-$conn = mysqli_connect('localhost','root','123456','test');
-if($conn){
-    echo 'ok';
-}else{
-    echo 'Not connected';
-}
+<html>
+<head>
+    <title>test ajax technology</title>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <style>
+    .container{
+        width: 960px;
+        margin: 0 auto;
+    }
+    .menu{
+        text-align: center;
+    }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="menu">
+        <button onclick="ajaxContent()">Нажми меня!</button>
+    </div>
+    <div class="ajax-result"></div>
+</div>
+<script>
+    $(document).ready(function(){
+        $('.menu').css({
+            'width': 960,
+            'heigth': 100
+        });
+    });
+    function ajaxContent(){
+        $.post('/ajax.php',{data: 'testAjax',length: '123467'},function(result){
+            console.log(result);
+            $('.ajax-result').text(result.test1);
+        },'JSON');
+    }
+</script>
+</body>
+</html>
